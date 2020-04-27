8918c4d799e260839fa29a56f053aaa146e58a40
unknown <18257346778@163.com>
2020-04-27 18:16:09 +0800

    fa

--- src/routes/api/orc.js
+++ src/routes/api/orc.js
@@ -16,23 +16,23 @@ const {
 // 登入
 router.post('/ocridcard', async (ctx, next) => {
   const {url, type} = ctx.request.body
-  let res = await axios.post(`https://api.weixin.qq.com/cv/ocr/idcard?type=${type}&img_url=${url}&access_token=${global.access_token}`, {
-    type,
-    img_url: url,
-    access_token: global.access_token
-  })
-  ctx.body = new SuccessModel(res.data)
-  // let res = await request.post(API_URL, {
-  //   api_key: API_KEY,
-  //   api_secret: API_SECRET,
-  //   image_url: url
+  // let res = await axios.post(`https://api.weixin.qq.com/cv/ocr/idcard?type=${type}&img_url=${url}&access_token=${global.access_token}`, {
+  //   type,
+  //   img_url: url,
+  //   access_token: global.access_token
   // })
-  // res = JSON.parse(res)
-  // if (res.error_message) {
-  //   throw new global.HttpException('身份证解析失败')
-  // } else{
-  //   ctx.body = new SuccessModel(res.cards[0])
-  // }
+  // ctx.body = new SuccessModel(res.data)
+  let res = await request.post(API_URL, {
+    api_key: API_KEY,
+    api_secret: API_SECRET,
+    image_url: url
+  })
+  res = JSON.parse(res)
+  if (res.error_message) {
+    throw new global.HttpException('身份证解析失败')
+  } else{
+    ctx.body = new SuccessModel(res.cards[0])
+  }
 })
 
 module.exports = router
